7ab289f95883a45fcbade3057867d81dbcf9a530
MINHAS3 <66636514+MINHAS3@users.noreply.github.com>
2020-11-02 12:02:38 -0600

    JS logicstep2

---
+++ logicStep2.js
@@ -0,0 +1,106 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Satellite: satellite,
+    Light: light
+};
+let earthquakes = new L.layerGroup();
+let tectonic_plates = new L.layerGroup(); 
+let overlays = {
+    Earthquakes: earthquakes, 
+    Tectonic_Plates : tectonic_plates
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+
+        // We turn each feature into a circleMarker on the map.
+
+        pointToLayer: function (feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
+
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.properties.mag),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+    } function getColor(magnitude) {
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    }
+
+});
+
+let boundaries = "https://raw.githubusercontent.com/minhas3/Mapping_Earthquakes/master/PB2002_boundaries.json";
+// Retrieve the earthquake GeoJSON data.
+d3.json(boundaries).then(function (data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(tectonic_plates);
+    tectonic_plates.addTo(map);
+});
